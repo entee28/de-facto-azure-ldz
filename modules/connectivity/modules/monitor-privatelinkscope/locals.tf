6536a4b86055ea19dfdb6380165e4eec274e086d
Thanh Nguyen <puffle.nnt@gmail.com>
2025-03-25 18:32:22 +0700

    Fix private endpoint IP configurations to handle empty cases gracefully

--- modules/connectivity/modules/monitor-privatelinkscope/locals.tf
+++ modules/connectivity/modules/monitor-privatelinkscope/locals.tf
@@ -3,12 +3,12 @@ locals {
 
   private_endpoint_ip_configurations = {
     for pe_key, pe_value in var.private_endpoint : pe_key => merge(pe_value, {
-      ip_configurations = length(pe_value.ip_configurations) > 0 ? {
+      ip_configurations = pe_value.ip_configurations != {} ? {
         for ip_key, ip_value in pe_value.ip_configurations : ip_key => merge(ip_value, {
           subresource_name = "azuremonitor"
           member_name      = "azuremonitor"
         })
-      } : pe_value.ip_configurations
+      } : {}
     })
   }
 }
