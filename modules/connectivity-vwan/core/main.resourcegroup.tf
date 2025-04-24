c40522452ca0a4fd156959a84473197b698f4033
Thanh Nguyen <puffle.nnt@gmail.com>
2025-04-24 19:22:12 +0700

    Refactor vwan_resourcegroup module to use for_each for resource group names

--- modules/connectivity-vwan/core/main.resourcegroup.tf
+++ modules/connectivity-vwan/core/main.resourcegroup.tf
@@ -1,9 +1,17 @@
 module "vwan_resourcegroup" {
+  for_each = {
+    "vwan-rg" = {
+      name = local.resource_group_name
+    }
+    "shared-services-rg" = {
+      name = local.shared_services_rg_name
+    }
+  }
   source           = "Azure/avm-res-resources-resourcegroup/azurerm"
   version          = "0.2.1"
   enable_telemetry = false
 
   location = var.location
-  name     = local.resource_group_name
+  name     = each.value.name
   tags     = var.resource_group_tags
 }
