afcaa18e8415eb1f4c50dc05185fa0bf397fa8c5
Thanh Nguyen <puffle.nnt@gmail.com>
2025-05-05 11:46:08 +0700

    refactor: Uncomment private DNS resolver module configuration in main.tf

--- connectivity-vwan/main.tf
+++ connectivity-vwan/main.tf
@@ -4,34 +4,34 @@ module "core" {
   company_name    = local.company_name
   location        = local.location
 
-  virtual_hub = local.core.virtual_hub
-  # firewall                            = local.core.firewall
-  # express_route_gateway               = local.core.express_route_gateway
-  # vpn_gateway                         = local.core.vpn_gateway
+  virtual_hub                         = local.core.virtual_hub
+  firewall                            = local.core.firewall
+  express_route_gateway               = local.core.express_route_gateway
+  vpn_gateway                         = local.core.vpn_gateway
   connectivity_sidecar_resourcegroups = local.core.connectivity_sidecar_resourcegroups
   sidecar_vnets                       = local.core.sidecar_vnets
   firewall_policy                     = local.core.firewall_policy
 }
 
-# module "private_dns_resolver" {
-#   source          = "./modules/shared-services/private-dns-resolver"
-#   subscription_id = local.subscription_id
-#   company_name    = local.company_name
-#   location        = local.location
+module "private_dns_resolver" {
+  source          = "./modules/shared-services/private-dns-resolver"
+  subscription_id = local.subscription_id
+  company_name    = local.company_name
+  location        = local.location
 
-#   inbound_endpoints = {
-#     inbound = {
-#       subnet_name                  = local.name.dns_resolver_inbound_subnet_name
-#       private_ip_allocation_method = "Static"
-#       private_ip_address           = "172.29.2.4"
-#     }
-#   }
-#   outbound_endpoints = {
-#     outbound = {
-#       subnet_name = local.name.dns_resolver_outbound_subnet_name
-#     }
-#   }
-# }
+  inbound_endpoints = {
+    inbound = {
+      subnet_name                  = local.name.dns_resolver_inbound_subnet_name
+      private_ip_allocation_method = "Static"
+      private_ip_address           = "172.29.2.4"
+    }
+  }
+  outbound_endpoints = {
+    outbound = {
+      subnet_name = local.name.dns_resolver_outbound_subnet_name
+    }
+  }
+}
 
 module "private_link_private_dns_zones" {
   source          = "./modules/shared-services/private-link-private-dns-zones"
