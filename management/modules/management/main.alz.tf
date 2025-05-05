b24e6c9c64f26107b04138fef4e865a47ff45f21
Thanh Nguyen <puffle.nnt@gmail.com>
2025-05-05 09:58:30 +0700

    refactor: Comment out unused azapi_client_config and module configuration in main.alz.tf

--- management/modules/management/main.alz.tf
+++ management/modules/management/main.alz.tf
@@ -1,11 +1,11 @@
-data "azapi_client_config" "current" {}
+# data "azapi_client_config" "current" {}
 
-module "alz" {
-  source           = "Azure/avm-ptn-alz/azurerm"
-  version          = "0.12.0"
-  enable_telemetry = false
+# module "alz" {
+#   source           = "Azure/avm-ptn-alz/azurerm"
+#   version          = "0.12.0"
+#   enable_telemetry = false
 
-  architecture_name  = "stb"
-  parent_resource_id = data.azapi_client_config.current.tenant_id
-  location           = var.location
-}
+#   architecture_name  = "stb"
+#   parent_resource_id = data.azapi_client_config.current.tenant_id
+#   location           = var.location
+# }
